f40f32279d027c2fb4ec14c56c73f829311f64a0
Admin <hebamahm65@gmail.com>
2026-05-17 12:27:32 +0300

    Heba Mahmoud

--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -13,4 +13,9 @@ public class TestCases {
     {
         System.out.println("another test case");
     }
+
+    @Test
+    public void HebaTestCase(){
+        System.out.println("Heba Mahmoud");
+    }
 }
